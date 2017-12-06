1f945dce5a75cacc187c7c96a58b0fcddf1e509b
jswett <jswett@sccs.net>
2017-12-05 18:26:14 -0800

    day 2 code

---
+++ lessons/day2.12.05.a.py
@@ -0,0 +1,56 @@
+# Math operations
+# Warm up:
+helloMessage = "Day 2: Numbers and operations"
+print(helloMessage)
+#Add
+print("When I add 5 + 12 =", 5 + 12)
+print("When I add 5 + -12 =", 5 + -12)
+foo = 47
+bar = 69
+print("when I add foo + bar =", foo + bar)
+
+#Subtract
+print("When I subtract 16 - 12 =", 16-12)
+print("When I subtract foo - bar =", foo-bar)
+
+print(" 2+4*3 =", 2+4*3)
+
+print("  4 - 3 - 2=", 4 - 3 - 2)
+print("4 - (3 - 2)=", 4 - (3 - 2))
+
+#Grouping and using parens for order of operations
+print("(2+4)*3 =", (2+4)*3)
+
+#Multiplication, *
+bar = 497672
+foo = 2
+print("When foo * bar = ", foo*bar)
+
+
+#Exponent, **
+base = 2
+exponent = 5
+print("When I raise", base, "to the", exponent,
+      "power = ", base**exponent)
+
+base=2
+exponent=-1
+print("2 to the -1 = ", base**exponent)
+
+#Divide, float / versus int //
+num = 5
+denom = 3
+print("num / denom", num/denom)
+
+# denom = 0
+# print("num / denom", num/denom)
+
+# integer division
+print("5//2=", 5//2)
+
+# print(3/0) is bad ZeroDivisionError
+
+
+
+
+# Multiply
